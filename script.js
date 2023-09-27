8e11adcfd94e9f7830346652dd69826ef66c72e8
perezmarcella <marcella.perez@yahoo.com.br>
2023-09-27 14:08:47 -0300

    primeiro commit sorteador o hamster da sorte

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector(".button")
+
+function generateNumber() {
+      
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+      
+    alert(result)
+}
+
+button.addEventListener("click", generateNumber)  
+  
+
