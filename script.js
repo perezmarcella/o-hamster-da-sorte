67f648a6e2546eea58438c6c8f983a3895fde119
perezmarcella <marcella.perez@yahoo.com.br>
2023-10-01 21:02:41 -0300

    inclusão do resultado na própria página

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ function generateNumber() {
       
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
+    const numberResult = document.querySelector(".result")
 
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
-      
-    alert(result)
+    
+    numberResult.innerHTML = result
+
+
+    
 }
 
 button.addEventListener("click", generateNumber)  
