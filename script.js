a86c601238dd4ba65490fcd271506f1fc060949b
perezmarcella <marcella.perez@yahoo.com.br>
2023-10-02 13:40:13 -0300

    implementação botão zerar inputs

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const button = document.querySelector(".button")
+const resetButton = document.querySelector(".reset-button")
+const begin = document.querySelector(".input-min")
+const end = document.querySelector(".input-max")
+const numberResult = document.querySelector(".result")
+
 
 function generateNumber() {
-      
-    const min = Math.ceil(document.querySelector(".input-min").value)
-    const max = Math.floor(document.querySelector(".input-max").value)
-    const numberResult = document.querySelector(".result")
-
+    const min = Math.ceil(begin.value)
+    const max = Math.floor(end.value)
+    
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
     
     numberResult.innerHTML = result
+   
+}
 
+function resetNumber() {
+    begin.value = ""
+    end.value = ""
 
-    
+    numberResult.innerHTML = "Resultado"
 }
 
 button.addEventListener("click", generateNumber)  
-  
+resetButton.addEventListener("click", resetNumber) 
 
